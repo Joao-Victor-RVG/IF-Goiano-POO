32a39ac509d9e8c2406638b01e32357c28642bae
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2023-02-15 20:17:45 -0300

    Lista 001, ex 003

---
+++ Exercises/Lista 001/ex 003/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+
+        System.out.println("Digite numeros aleatorios e quando quiser parar digite (-1)");
+
+        while (true) {
+            int num = scanner.nextInt();
+
+            if (num == -1) {
+                break;
+            }
+
+            count++;
+            sum += num;
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("Valor invalido");
+        } else {
+            double media = (double) sum / count;
+
+            System.out.println("A quantidade de números digitados pelo usuário; " + count);
+            System.out.println("O maior número digitado; " + max);
+            System.out.println("O menor número digitado;" + min);
+            System.out.println("A média dos números digitados; " + media);
+        }
+
+        scanner.close();
+    }
+}
